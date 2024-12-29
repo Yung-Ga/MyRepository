912460ffc6985b35a7264daba0920432806f2981
Yung-Ga <dus1655@gmail.com>
2024-12-29 19:07:17 +0900

    Modify devB

--- GitTask/src/Git/A.java
+++ GitTask/src/Git/A.java
@@ -3,5 +3,6 @@ package Git;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Class A!");
+		System.out.println("Modify class A in B"); 
     }
 }
--- GitTask/src/Git/B.java
+++ GitTask/src/Git/B.java
@@ -3,5 +3,6 @@ package Git;
 public class B {
 	public static void main(String[] args) {
 		System.out.println("Class B!");
+		System.out.println("Modify class B in B"); 
 	}
 }
